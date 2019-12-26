de40079fe0f44dd5662daff0f7e3455410db9b2f
ayasamau <aseprudini46@gmail.com>
2019-12-26 20:51:35 +0700

    contoh text editor

---
+++ texteditor/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contoh</title>
+	<script type="text/javascript" src="ckeditor/ckeditor.js"></script>
+</head>
+<body>
+<textarea class="ckeditor " id="ckedtor" name="deskripsi" required autocomplete="deskripsi" placeholder="Deskripsi"></textarea>
+</body>
+</html>
