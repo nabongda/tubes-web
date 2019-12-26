e3227f747210b6d8667f4cf6c53fed2bb165a965
nabongda <nadaqonita01@gmail.com>
2019-12-26 20:12:03 +0700

    kirimm proses registrasi

---
+++ registrasi/registrasi.php
@@ -0,0 +1,39 @@
+<?php
+
+include_once "../koneksi.php";
+
+$nama=$_POST['nama'];
+$username=$_POST['username'];
+$ttl=$_POST['ttl'];
+$jk=$_POST['jk'];
+$password=$_POST['password'];
+$kpassword=$_POST['kpassword'];
+
+$cek_user=mysqli_num_rows(mysqli_query($mysqli, "SELECT * FROM user WHERE username='$username'"));
+
+if ($cek_user > 0) {
+    echo '<script language="javascript">
+    alert ("Username Sudah Ada Yang Menggunakan");
+    window.location="index.php";
+    </script>';
+    exit();
+}
+
+else {
+    if($password == $kpassword){
+        $level="warga";
+        $result=mysqli_query($mysqli, "INSERT INTO user(nama_user, username, tanggal_lahir, jenis_kelamin, kata_sandi, level) VALUES ('$nama','$username','$ttl','$jk','$password', '$level')");
+
+        echo '<script language="javascript">
+        alert ("Registrasi Berhasil Di Lakukan!");
+        window.location="index.php";
+        </script>';
+        exit();
+    } 
+    else{
+        echo "<script>alert('Password yang Anda Masukan Tidak Sama');history.go(-1)</script>";
+    }
+    
+}
+
+?>
